e3d48803b2f79b82914e9d6498e11594b18c04d0
irhayeelvis <tony69elvis@gmail.com>
2026-01-02 12:55:53 +0100

    updated file: calculator logic without front end. issue#2

---
+++ simple calculator logic.js
@@ -0,0 +1,27 @@
+let operator1 = Number(prompt(`Number One`));
+console.log(operator1);
+let operator = prompt(`Equation`)
+let operator2 = Number(prompt(`Number Two`));
+console.log(operator2);
+let total;
+function calculator(x = 2, o = "+", y = 3) {
+    if (o === "+") {
+        total = x + y;
+    } else if (o === "-") {
+        total = x - y;
+    } else if (o === "*") {
+        total = x * y;
+    } else if (o === "/") {
+        total = x / y;
+    } else {
+        total = x + y;
+    };
+
+    if (typeof(operator) !== String) {
+        alert(`${operator1} ${operator} ${operator2} = ${total}`);
+    } else {
+        alert(`${operator1} + ${operator2} = ${total}`);
+    };
+};
+
+calculator(operator1, operator, operator2);
