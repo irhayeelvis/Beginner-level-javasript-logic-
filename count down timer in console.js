b7cbb822535049deb2a04f28cc367709491e5acc
bitpunkxr <tony69elvis@gmail.com>
2026-02-01 04:08:22 +0100

    Updated countdown timer to paramedy launch

--- count down timer in console.js
+++ count down timer in console.js
@@ -1,24 +1,29 @@
-const endDate = "February 1, 2026";
+// Count down timer to launch paramedy
+
+const endDate = "April 29, 2026";
 function countdown() {
- const total = Date.parse(endDate) - new Date();
- const days = Math.floor(total / (1000 * 60 * 60 * 24));
- const hrs = Math.floor((total / (1000 * 60 * 60)) % 24);
- const mins = Math.floor((total / 1000 / 60) % 60);
- const secs = Math.floor((total / 1000) % 60);
- return {
- days,
- hrs,
- mins,
- secs
- };
-}
+    const date = new Date();
+    const total = Date.parse(endDate) - date;
+    const days  = Math.floor(total / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const seconds = Math.floor((total / 1000) % 60);
+    return {
+        days,
+        hours,
+        minutes,
+        seconds
+    };
+};
+
 function update() {
- const temp = countdown();
- let output = "";
- for (const property in temp) {
- output += (`${property}: ${temp[property]} `);
- }
- console.log(output);
- setTimeout(update, 1000);
+    let temp = countdown();
+    let output = "";
+    for (const property in temp) {
+        output += (`${property}:${temp[property]} `);
+    };
+    console.log(output);
+    setTimeout(update, 1000);
 }
+
 update();
